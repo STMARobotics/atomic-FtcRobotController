beebfb2707954a1aa422574229284e3ed6a06dee
owen <me6apl1er@gmail.com>
2024-10-23 21:53:44 -0500

    OH YEAHH, VECTORRRRR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControl.java
@@ -10,14 +10,27 @@ public class ArmControl {
     private double armTargetPosition = 0.0;
 
     // PID constants
+    private static final double kP = 0.01;
+    private static final double kI = 0.001;
+    private static final double kD = 0;
+    private static final double kF = 0;
 
     public ArmControl(HardwareMap hardwareMap) {
         arm = new MotorEx(hardwareMap, "arm");
         armPID = new PIDFController(kP, kI, kD, kF);
     }
 
+    public void setJoystickInput(double joystickInput) {
+        armTargetPosition += -joystickInput * 5;
+    }
+
+    public void update() {
         double armPower = armPID.calculate(arm.getCurrentPosition(), armTargetPosition);
         arm.set(armPower);
+    }
+
+    public void setPosition(double position){
+        armTargetPosition = position;
     }
 
     public double getArmTargetPosition() {
@@ -27,4 +40,16 @@ public class ArmControl {
     public double getArmPower() {
         return armPID.calculate(arm.getCurrentPosition(), armTargetPosition);
     }
+    public void resetZero(){
+        arm.stopAndResetEncoder();
+    }
+
+    public double getArmPosition(){
+        return arm.getCurrentPosition();
+    }
+
+    public void setArmPower(double power){
+        arm.set(power);
+    }
+
 }
