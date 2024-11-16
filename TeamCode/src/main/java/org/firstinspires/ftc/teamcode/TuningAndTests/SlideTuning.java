fca492f440379e9680fa72b27b0cf1067623fd4d
owen <me6apl1er@gmail.com>
2024-11-16 13:57:23 -0600

    fixed pid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuningAndTests/SlideTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuningAndTests/SlideTuning.java
@@ -2,59 +2,53 @@ package org.firstinspires.ftc.teamcode.TuningAndTests;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.SubSystems.SlideControl;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @TeleOp(name="Slide Control TeleOp", group="TeleOp")
 public class SlideTuning extends LinearOpMode {
 
     private SlideControl slideControl;  // Renamed subsystem
-    private DcMotorEx slideMotor;
+    private DcMotorEx slide;
     private Servo servo;
-
+    double targetPosition;
     @Override
     public void runOpMode() throws InterruptedException {
 
-        // Initialize hardware
-        slideMotor = hardwareMap.get(DcMotorEx.class, "slideMotor");
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
         servo = hardwareMap.get(Servo.class, "servo");
 
-        // Initialize SlideControl subsystem
-        slideControl = new SlideControl(slideMotor, servo);
+        slideControl = new SlideControl(slide, servo);
 
-        // Set initial servo position at start of teleop
         slideControl.setServoPosition(65);
-
-        // Reset encoder and set the initial position as zero
         slideControl.resetEncoder();
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            // Control slide motor with left stick Y-axis on gamepad 2
-            if (gamepad2.left_stick_y != 0) {
-                // Adjust target position based on the Y-axis
-                double delta = gamepad2.left_stick_y * 5;  // Increase/decrease by 5 degrees
-                slideControl.setTargetPosition(slideControl.getTargetPosition() + delta);
-            }
+        if (gamepad2.left_stick_y > 0.1) {
+                targetPosition += 10;
+            } else if (gamepad2.left_stick_y < -0.1) {
+                targetPosition -= 10;
+        }
 
+        slideControl.setTargetPosition(targetPosition);
             // Control the servo positions with D-pad
             if (gamepad2.dpad_up) {
-                slideControl.setServoPosition(-10);
+                slideControl.setServoPosition(-10);  // Set to -10 degrees
             } else if (gamepad2.dpad_right) {
-                slideControl.setServoPosition(65);
+                slideControl.setServoPosition(65);  // Set to 65 degrees
             } else if (gamepad2.dpad_down) {
-                slideControl.setServoPosition(-80);
+                slideControl.setServoPosition(-80);  // Set to -80 degrees
             }
 
-            // Update PID control and telemetry
-            slideControl.update(telemetry);
+            slideControl.update();
+            slideControl.updateTelemetry(telemetry);
 
-            // Wait for a short time to prevent a busy loop
             sleep(20);
         }
     }
