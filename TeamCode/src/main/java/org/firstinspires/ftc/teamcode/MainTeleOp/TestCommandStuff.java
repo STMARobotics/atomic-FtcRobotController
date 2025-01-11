73e6dc3e452682f3423317c26094aad2c9747f9b
Owen Rossing <151468667+ME6A-PL1ER@users.noreply.github.com>
2025-01-10 18:16:58 -0600

    de chatgptify the testcommandstuff thing that I couldnt figure out how to do on my own

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp/TestCommandStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp/TestCommandStuff.java
@@ -13,32 +13,27 @@ import org.firstinspires.ftc.teamcode.SubSystems.ArmControl;
 import org.firstinspires.ftc.teamcode.SubSystems.SlideControl;
 import org.firstinspires.ftc.teamcode.SubSystems.MainSubsystem;
 
-@TeleOp(name = "My TeleOp")
+@TeleOp(name = "TestCommandStuff")
 public class TestCommandStuff extends CommandOpMode {
     private GamepadEx driverOp;
     private testSlideUp command1;
     private testMoveCommand command2;
 
-    // Declare subsystems
     private ArmControl armControl;
     private SlideControl slideControl;
     private MainSubsystem mainSubsystem;
 
     @Override
     public void initialize() {
-        // Initialize gamepad
         driverOp = new GamepadEx(gamepad1);
 
-        // Initialize subsystems
         armControl = new ArmControl(hardwareMap);
         slideControl = new SlideControl(hardwareMap);
         mainSubsystem = new MainSubsystem(hardwareMap);
 
-        // Initialize commands with subsystems
         command1 = new testSlideUp(armControl, slideControl, mainSubsystem);
         command2 = new testMoveCommand(slideControl, armControl, mainSubsystem);
 
-        // Create button binding
         new GamepadButton(driverOp, GamepadKeys.Button.A)
                 .whenPressed(
                         new ParallelCommandGroup(
