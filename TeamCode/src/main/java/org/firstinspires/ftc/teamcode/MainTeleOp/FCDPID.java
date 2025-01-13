42f8c30dde581d50df6f5dc8e42a93c43a2fd9a8
Owen Rossing <151468667+ME6A-PL1ER@users.noreply.github.com>
2025-01-13 10:59:08 -0600

    Update FCDPID.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp/FCDPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp/FCDPID.java
@@ -25,6 +25,7 @@ public class FCDPID extends LinearOpMode {
     private DcMotor rearRight;
     private DcMotor rearLeft;
     private DcMotor frontLeft;
+    private DcMotor frontRight
     private IMU imu;
     private int presetCycle = 0;
     private double lastPresetCycle = 0;
